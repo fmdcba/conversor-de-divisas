222b83fc69893eba350585a6732a698a06ea1a94
Facundo Manuel Díaz Córdoba <facudiaz1738@hotmail.com>
2024-04-05 16:52:31 -0300

    agrega y configura linter y formatter

--- src/index.js
+++ src/index.js
@@ -1,31 +1,30 @@
-const API = "https://api.frankfurter.app";
-const $tabla = document.querySelector(".table #listado");
+const API = 'https://api.frankfurter.app';
+const $tabla = document.querySelector('.table #listado');
 
 function obtenerMonedas() {
   fetch(`${API}/currencies`)
     .then((respuesta) => respuesta.json())
     .then((respuestaJSON) => {
       const Monedas = Object.entries(respuestaJSON);
-
       mostrarMonedas(Monedas);
     })
-    .catch((error) => console.log("Falló", error));
+    .catch((error) => console.log('Falló', error));
 }
 
 function mostrarMonedas(monedas) {
   for (const [simbolo, moneda] of monedas) {
     mostrarSimbolosBase(simbolo);
     mostrarSimbolosConversor(simbolo);
-    const $fila = document.createElement("tr");
-    const $simbolo = document.createElement("th");
-    const $moneda = document.createElement("td");
-    const $valor = document.createElement("td");
+    const $fila = document.createElement('tr');
+    const $simbolo = document.createElement('th');
+    const $moneda = document.createElement('td');
+    const $valor = document.createElement('td');
     $fila.id = simbolo;
     $valor.id = `valor-${simbolo}`;
-    $valor.className = "valor";
+    $valor.className = 'valor';
     $simbolo.textContent = simbolo;
     $moneda.textContent = moneda;
-    $valor.textContent = "#";
+    $valor.textContent = '#';
 
     $tabla.appendChild($fila);
     $fila.appendChild($simbolo);
@@ -35,20 +34,20 @@ function mostrarMonedas(monedas) {
 }
 
 function mostrarSimbolosBase(simbolo) {
-  const $contenedorBases = document.querySelector("#simbolo-base");
-  const $opcionBase = document.createElement("option");
+  const $contenedorBases = document.querySelector('#simbolo-base');
+  const $opcionBase = document.createElement('option');
   $opcionBase.textContent = simbolo;
 
   $contenedorBases.appendChild($opcionBase);
 }
 
 function mostrarSimbolosConversor(simbolo) {
-  const $contenedorBaseDe = document.querySelector("#convertir-de");
-  const $contenedorBaseA = document.querySelector("#convertir-a");
-  const $opcionBaseDe = document.createElement("option");
+  const $contenedorBaseDe = document.querySelector('#convertir-de');
+  const $contenedorBaseA = document.querySelector('#convertir-a');
+  const $opcionBaseDe = document.createElement('option');
   $opcionBaseDe.textContent = simbolo;
   $opcionBaseDe.value = simbolo;
-  const $opcionBaseA = document.createElement("option");
+  const $opcionBaseA = document.createElement('option');
   $opcionBaseA.textContent = simbolo;
   $opcionBaseA.value = simbolo;
 
@@ -56,21 +55,21 @@ function mostrarSimbolosConversor(simbolo) {
   $contenedorBaseA.appendChild($opcionBaseA);
 }
 
-document.querySelector("#listar").onclick = function (e) {
-  const $mensajeFecha = document.querySelector("#mensaje-fecha");
-  $mensajeFecha.textContent = "";
+document.querySelector('#listar').onclick = function (e) {
+  const $mensajeFecha = document.querySelector('#mensaje-fecha');
+  $mensajeFecha.textContent = '';
 
   manejarBase();
   e.preventDefault();
 };
 
 function manejarBase() {
-  const $simbolo = document.querySelector("#simbolo-base");
+  const $simbolo = document.querySelector('#simbolo-base');
   const simbolo = $simbolo.value;
   const $valor = document.querySelector(`#valor-${simbolo}`);
   const $filaValor = document.querySelector(`#${simbolo}`);
-  $filaValor.className = "table-light";
-  $valor.textContent = "$1";
+  $filaValor.className = 'table-light';
+  $valor.textContent = '$1';
 
   actualizarValores(simbolo);
 }
@@ -83,7 +82,7 @@ function actualizarValores(base) {
 
       mostrarValores(valores);
     })
-    .catch((error) => console.log("Falló", error));
+    .catch((error) => console.log('Falló', error));
 }
 
 function mostrarValores(valores) {
@@ -96,34 +95,34 @@ function mostrarValores(valores) {
     }
 
     if ($filaValor !== null) {
-      if ($filaValor.classList.contains("table-light")) {
-        $filaValor.classList.remove("table-light");
+      if ($filaValor.classList.contains('table-light')) {
+        $filaValor.classList.remove('table-light');
       }
     }
   }
 }
 
-document.querySelector("#filtrar-fecha").onclick = function (e) {
+document.querySelector('#filtrar-fecha').onclick = function (e) {
   filtrarPorFecha();
   e.preventDefault();
 };
 
 function filtrarPorFecha() {
-  const $anio = document.querySelector("#anio");
-  const $mes = document.querySelector("#mes");
-  const $dia = document.querySelector("#dia");
-  const $simbolo = document.querySelector("#simbolo-base");
+  const $anio = document.querySelector('#anio');
+  const $mes = document.querySelector('#mes');
+  const $dia = document.querySelector('#dia');
+  const $simbolo = document.querySelector('#simbolo-base');
   const anio = Number($anio.value);
   let mes = Number($mes.value);
   let dia = Number($dia.value);
   const simbolo = $simbolo.value;
 
   if (mes < 10) {
-    mes = ("0" + mes).slice(-2);
+    mes = ('0' + mes).slice(-2);
   }
 
   if (dia < 10) {
-    dia = ("0" + dia).slice(-2);
+    dia = ('0' + dia).slice(-2);
   }
 
   obtenerValoresPorFecha(anio, mes, dia, simbolo);
@@ -133,41 +132,41 @@ function obtenerValoresPorFecha(anio, mes, dia, simbolo) {
   fetch(`${API}/${anio}-${mes}-${dia}?from=${simbolo}`)
     .then((respuesta) => respuesta.json())
     .then((respuestaJSON) => {
-      const $mensaje = document.querySelector("#mensaje-fecha");
+      const $mensaje = document.querySelector('#mensaje-fecha');
       resultado = respuestaJSON;
 
       if (resultado.message) {
-        $mensaje.textContent = "No encontrado";
-        $mensaje.className = "badge text-bg-danger";
+        $mensaje.textContent = 'No encontrado';
+        $mensaje.className = 'badge text-bg-danger';
         reiniciarValores();
       } else {
         valores = Object.entries(resultado.rates);
 
         $mensaje.textContent = `Valores filtrados en fecha ${resultado.date} en base a $${resultado.amount}-${simbolo}`;
-        $mensaje.className = "badge text-bg-success";
+        $mensaje.className = 'badge text-bg-success';
 
         reiniciarValores();
         mostrarValores(valores);
       }
     })
-    .catch((error) => console.log("Falló", error));
+    .catch((error) => console.log('Falló', error));
 }
 
 function reiniciarValores() {
-  document.querySelectorAll(".valor").forEach((valor) => {
-    valor.textContent = "#";
+  document.querySelectorAll('.valor').forEach((valor) => {
+    valor.textContent = '#';
   });
 }
 
-document.querySelector("#convertir").onclick = function (e) {
+document.querySelector('#convertir').onclick = function (e) {
   obtenerValoresParaConversion();
   e.preventDefault();
 };
 
 function obtenerValoresParaConversion() {
-  const cantidad = document.querySelector("#cantidad").value;
-  const convertirDe = document.querySelector("#convertir-de").value;
-  const convertirA = document.querySelector("#convertir-a").value;
+  const cantidad = document.querySelector('#cantidad').value;
+  const convertirDe = document.querySelector('#convertir-de').value;
+  const convertirA = document.querySelector('#convertir-a').value;
 
   convertirMoneda(cantidad, convertirDe, convertirA);
 }
@@ -181,13 +180,13 @@ function convertirMoneda(cantidad, monedaDe, monedaA) {
 }
 
 function mostrarResultadoConversion(resultado) {
-  const $mensajeResultado = document.querySelector("#resultado-conversion");
+  const $mensajeResultado = document.querySelector('#resultado-conversion');
   $mensajeResultado.textContent = `$${resultado.amount}-"${
     resultado.base
   }" equivale a $${Object.values(resultado.rates)}-"${Object.keys(
     resultado.rates,
   )}" a fecha ${resultado.date}`;
-  $mensajeResultado.className = "badge text-bg-success";
+  $mensajeResultado.className = 'badge text-bg-success';
 }
 
 obtenerMonedas();
